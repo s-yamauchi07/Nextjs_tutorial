e6734a580360c1569af903d05192125856fad393
s-yamauchi07 <hand1nhand306@gmail.com>
2025-07-01 21:55:17 +0900

    latestInvoicesをストリーミング

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -2,14 +2,11 @@ import { Card } from '@/app/ui/dashboard/cards';
 import RevenueChart from '@/app/ui/dashboard/revenue-chart';
 import LatestInvoices from '@/app/ui/dashboard/latest-invoices';
 import { lusitana } from '@/app/ui/font';
-import { fetchLatestInvoices,fetchCardData } from '../../lib/data';
+import { fetchCardData } from '../../lib/data';
 import { Suspense } from 'react'; 
-import { RevenueChartSkeleton } from '@/app/ui/skeletons';
+import { RevenueChartSkeleton, LatestInvoicesSkeleton } from '@/app/ui/skeletons';
 
 export default async function Page() {
-  // const revenue = await fetchRevenue();
-
-  const latestInvoices = await fetchLatestInvoices();
   const { 
     totalPaidInvoices, 
     totalPendingInvoices, 
@@ -36,7 +33,9 @@ export default async function Page() {
         <Suspense fallback={<RevenueChartSkeleton />}>
           <RevenueChart />
         </Suspense>
-        <LatestInvoices latestInvoices={latestInvoices} />
+        <Suspense fallback={<LatestInvoicesSkeleton />}>
+          <LatestInvoices />
+        </Suspense>
       </div>
     </main>
   );
